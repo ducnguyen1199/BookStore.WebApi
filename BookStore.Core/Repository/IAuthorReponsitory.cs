da1d8c3fa8650ba1998352cbea41e3d1b668d8b1
ducdzn <congduc1199@gmail.com>
2021-05-27 09:10:00 +0700

    ok

--- BookStore.Core/Repository/IAuthorReponsitory.cs
+++ BookStore.Core/Repository/IAuthorReponsitory.cs
@@ -8,6 +8,7 @@ namespace BookStore.Core.Repository
 	public interface IAuthorReponsitory: IRepository
 	{
 		Task<ListItemResponse<Author>> Get();
+		Task<Author> Get(int id);
 		Task<Author> Add(Author author);
 		Task Delete(int id);
 		Task Update(int Id, Author author);
